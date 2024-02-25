7dc8a2cc30dc1f4165e95257cae433c3c3c146d8
maedarenfutoshi <japan_lunch_fate@yahoo.co.jp>
2024-02-25 16:48:05 +0900

    Initial Commit

---
+++ WordApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordApp
+//
+//  Created by 前田蓮太 on 2024/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
